02c2a0e7c3f6a15adb9c1ae569eebfcc89d52ee6
ubonisrael <akpanudohubonisrael@gmail.com>
2024-04-24 19:46:00 +0100

    configured page and layout in app to incorporate changes

--- acme-education-frontend/app/page.tsx
+++ acme-education-frontend/app/page.tsx
@@ -1,9 +1,6 @@
+import { HomePage } from "./components/home";
 
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
-    </main>
-  );
+  return <HomePage/>;
 }
