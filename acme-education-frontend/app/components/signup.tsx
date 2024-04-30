e58996342e0bb0948741d61cea1dc30f6e45621a
ubonisrael <akpanudohubonisrael@gmail.com>
2024-04-30 18:19:50 +0100

    updated signup page

--- acme-education-frontend/app/components/signup.tsx
+++ acme-education-frontend/app/components/signup.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MyTextInput } from "./form";
+import { FieldSet, MyTextInput } from "./form";
 import { Formik, Form } from "formik";
 import { signupSchema } from "../validation/auth";
 
@@ -8,8 +8,10 @@ const SignUpForm = () => {
   return (
     <>
       <div className="w-full flex flex-col items-center justify-center gap-4 py-8 bg-white rounded-2xl shadow-xl">
-        <div className="w-full text-center font-urbanist">
-          <h2 className="font-semibold text-3xl md:text-4xl capitalize mb-2 md:mb-4">Sign Up</h2>
+        <div className="w-full text-center">
+          <h2 className="font-semibold text-3xl md:text-4xl capitalize mb-2 md:mb-4">
+            Sign Up
+          </h2>
           <p className="max-w-sm mx-auto">
             Join millions of institutions, teachers and students using ACME
             Education
@@ -23,6 +25,7 @@ const SignUpForm = () => {
             telephone: "",
             password: "",
             confirmPassword: "",
+            isTeacher: "",
           }}
           validationSchema={signupSchema}
           onSubmit={(values, { setSubmitting }) => {
@@ -69,9 +72,17 @@ const SignUpForm = () => {
               type="password"
               placeholder=""
             />
+            <FieldSet
+              label="Are you a Teacher or a Student?"
+              name="isTeacher"
+              options={[
+                { name: "isTeacher", label: "Teacher", value: true },
+                { name: "isTeacher", label: "Student", value: false },
+              ]}
+            />
             <button
               type="submit"
-              className="w-40 py-2 mt-4 bg-blue-100 text-black hover:text-white hover:bg-blue-700 capitalize rounded-3xl"
+              className="w-40 py-2 mt-8 bg-blue-100 text-black hover:text-white hover:bg-blue-700 capitalize rounded-3xl"
             >
               sign up
             </button>
