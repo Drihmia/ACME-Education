15b9b80731b906088d59605056fcee9fc66c0042
ubonisrael <akpanudohubonisrael@gmail.com>
2024-05-02 22:53:03 +0100

    modified pop.bash

--- pop.bash
+++ pop.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Enter your Database's password"
 read -r pwd
-D='ACME' u='root' h='localhost' p=$pwd python3 populate_states_cities_1.py
-D='ACME' u='root' h='localhost' p=$pwd python3 populate_states_cities_2.py
-D='ACME' u='root' h='localhost' p=$pwd python3 populate_institutions.py
-D='ACME' u='root' h='localhost' p=$pwd python3 main_test_2.py
+#D='ACME' u='root' h='localhost' p=$pwd python3 populate_states_cities_1.py
+#D='ACME' u='root' h='localhost' p=$pwd python3 populate_states_cities_2.py
+# D='ACME' u='root' h='localhost' p=$pwd python3 populate_institutions.py
+# D='ACME' u='root' h='localhost' p=$pwd python3 main_test_2.py
 D='ACME' u='root' h='localhost' p=$pwd python3 -m api.v1.app
