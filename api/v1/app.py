b51bc4ac3ac110e69ad22ef24418197bc9c11038
ubonisrael <akpanudohubonisrael@gmail.com>
2024-05-03 07:58:00 +0100

    extended data returned during login added jwt config variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Flask Application """
+from datetime import timedelta
 from flask import Flask, make_response
 from flask_cors import CORS
 from flasgger import Swagger
@@ -11,8 +12,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-jwt = JWTManager(app)
+
+# app.config["JWT_COOKIE_SECURE"] = False
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
 app.config["JWT_SECRET_KEY"] = '96389b9ffb48fcc169bd922991b420f9cb3e27e13a1cc6b188edc28745df08a7'
+jwt = JWTManager(app)
 
 
 @app.teardown_appcontext
