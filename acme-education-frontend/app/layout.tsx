503dae5c89e12b9be5d8eb500939bedfa6da6d2d
Drihmia <drihmia.redouane@gmail.com>
2024-07-22 18:36:37 +0000

    feat: Add BrevoWidget

--- acme-education-frontend/app/layout.tsx
+++ acme-education-frontend/app/layout.tsx
@@ -1,9 +1,11 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "./ui/header";
 import { Footer } from "./ui/footer";
 import { AuthUserProvider } from "@/app/context/authContext";
+import BrevoWidget from "@/app/components/BrevoWidget";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,13 +22,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title as string}</title>
+        <meta name="description" content={metadata.description as string} />
+      </head>
       <body className={inter.className}>
         <AuthUserProvider>
           <Header />
           {children}
           <Footer />
+          <BrevoWidget />
         </AuthUserProvider>
       </body>
     </html>
   );
 }
+
+
