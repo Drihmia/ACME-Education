c5e946012e7133418dd6d4ecbc69797ad71e0588
Drihmia <drihmia.redouane@gmail.com>
2024-07-22 19:06:42 +0000

    chore: enhance JWT configuration and CSRF protection
    - Added CSRF protection for JWT cookies.
    - Updated JWT cookie settings: secure, HTTP-only, same-site, and expiration.
    - Registered new blueprint for API documentation.
    - Updated API documentation URL.
    - /docs (https://drihmia.tech/docs).

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
 """ Flask Application """
 from datetime import timedelta
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 import json
 import os
 from models import storage
-from api.v1.views import app_views
+from api.v1.views import app_views, app_views_docs
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.register_blueprint(app_views_docs)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# app.config["JWT_COOKIE_SECURE"] = False
+# Location and name of cookie:
 app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
+app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token'
 
+# Setting life time of cookies
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+
+# Securing cookies:
+app.config['JWT_COOKIE_SECURE'] = True  # I'm using https for my webapp
+app.config['JWT_COOKIE_HTTPONLY'] = True
+app.config['JWT_COOKIE_SAMESITE'] = 'Lax'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 if not JWT_SECRET_KEY:
     print("JWT_SECRET_KEY is empty")
@@ -44,4 +54,4 @@ def not_found(_):
 if __name__ == "__main__":
     """Starts the API"""
     with app.app_context():
-        app.run(threaded=True)
+        app.run(threaded=True, debug=os.getenv("DEBUG", False))
