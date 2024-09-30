5357a23fdc849a8c56d99b53055826206ef15edf
Drihmia <drihmia.redouane@gmail.com>
2024-09-30 22:03:42 +0000

    fix: If not authenticated, always redirect to login page, for all pages that start with dashboard

--- acme-education-frontend/middleware.ts
+++ acme-education-frontend/middleware.ts
@@ -1,17 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const protectedRoutes = ['/dashboard']
-const teacherOnlyRoutes = ['/dashboard/lessons/add', '/dashboard/lessons/edit']
+const protectedRoutes = '/dashboard'
+// const teacherOnlyRoutes = ['/dashboard/lessons/add', '/dashboard/lessons/edit']
 const authRoutes = ['/', '/login', '/signup']
 
 
 export function middleware(request: NextRequest) {
     const currentUser = request.cookies.get("currentUser")?.value
 
-    if (protectedRoutes.includes(request.nextUrl.pathname) && (!currentUser)) {
+    if (request.nextUrl.pathname.startsWith(protectedRoutes) && (!currentUser)) {
         request.cookies.delete("currentUser")
+        request.cookies.delete("access_token")
         const response = NextResponse.redirect(new URL("/login", request.url))
         response.cookies.delete("currentUser")
+        response.cookies.delete("access_token")
 
         return response
     }
