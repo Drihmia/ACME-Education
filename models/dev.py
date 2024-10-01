9b0a0f030e91ca78b72fd812af242055bdf524eb
Drihmia <drihmia.redouane@gmail.com>
2024-10-01 12:41:56 +0100

    feat: Add dev model

---
+++ models/dev.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+""" holds class Teacher"""
+import bcrypt
+from sqlalchemy import Column, event, String
+from models.base_model import BaseModel, Base
+
+
+class Dev(BaseModel, Base):
+    """Representation of Developer """
+    __tablename__ = 'devs'
+
+    # -------------------------------------------------------------
+    # Normal attributes.
+    first_name = Column(String(64), nullable=False, default='dev')  # A must
+    last_name = Column(String(64), nullable=False, default='one')  # A must
+    email = Column(String(64), nullable=False, unique=True, default='dev1@gmail.com')  # A must
+    password = Column(String(64), nullable=False, default='dev1')  # A must
+
+    # -------------------------------------------------------------
+    # Optional attributes.
+    gender = Column(String(1), nullable=True)
+    phone_number = Column(String(14), nullable=True)
+
+
+
+def hash_password_before_insert_or_update(_, __, dev):
+    """Hashing the password before store it into database"""
+    if dev.password is not None and isinstance(dev.password, str):
+        # Generate a salt and hash the password using bcrypt
+        if len(dev.password) != 60:
+            salt = bcrypt.gensalt()
+            dev.password = bcrypt.hashpw(
+                dev.password.encode('utf-8'), salt)
+
+
+event.listen(Dev, 'before_insert', hash_password_before_insert_or_update)
+event.listen(Dev, 'before_update', hash_password_before_insert_or_update)
