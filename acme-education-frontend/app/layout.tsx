c582a72beadf38d0aab285b212ceb9dba1573bb4
Drihmia <drihmia.redouane@gmail.com>
2024-10-08 22:03:01 +0000

    feat: Add Google Tag Manager (gtag.js) to RootLayout for site analytics
     - Included Google Tag Manager script for tracking and analytics
    
     - Used Next.js Script component for optimized loading after interaction
    
     - Placed the gtag.js script right after the <head> element as recommended

--- acme-education-frontend/app/layout.tsx
+++ acme-education-frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // Import Script component
 import "./globals.css";
 import { Header } from "./ui/header";
 import { Footer } from "./ui/footer";
@@ -15,6 +16,8 @@ export const metadata: Metadata = {
     "An e-platform that aims to make the learning process easier for all involved.",
 };
 
+const googleTagId = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,6 +30,20 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title as string}</title>
         <meta name="description" content={metadata.description as string} />
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleTagId}');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <AuthUserProvider>
@@ -39,5 +56,4 @@ export default function RootLayout({
     </html>
   );
 }
-
 
